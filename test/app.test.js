1071b5666d4f3aaa9560b02a756c147869544bde
w3cj <cj@null.computer>
2018-08-20 19:34:52 -0400

    :bug: Remove failing test after adding docs to root.

--- test/app.test.js
+++ test/app.test.js
@@ -11,15 +11,3 @@ describe('app', () => {
       .expect(404, done);
   });
 });
-
-describe('GET /', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-      }, done);
-  });
-});
